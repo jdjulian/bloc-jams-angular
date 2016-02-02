16b2845279a9a6865851eddb472e608738c57284
Jean Juliano <jeanjuliano927@gmail.com>
2016-02-02 12:42:40 -0800

    Assignment 3 modified, completed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,8 +14,14 @@
             .state('album', {
                 url: '/album',
                 templateUrl: '/templates/album.html'
+        })
+// Assigngment 3 start----------
+        .state('collection', {
+                url: '/collection',
+                templateUrl: '/templates/collection.html'
         });
     }
+// Assigngment 3 finish----------
     
 angular
     .module('blocJams', ['ui.router'])
